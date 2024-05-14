bfdcc2c9ca0b36da7fe299d452ff2b152773e95c
Jan Madeyski <jan.madeyski@gmail.com>
2024-05-14 12:17:32 +0200

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div>
+    <nav class="topbar sticky top-0 bg-primary/5 flex flex-row items-center justify-between gap-2 border-b py-1 px-2">
+      <div class="font-bold">
+        App
+      </div>
+      <div class="navigation flex flex-row items-center justify-end gap-2 text-sm">
+        <RouterLink
+          class="link"
+          active-class="active"
+          to="/"
+        >
+          Home
+        </RouterLink>
+        <RouterLink
+          class="link"
+          active-class="active"
+          to="/about"
+        >
+          About
+        </RouterLink>
+        <RouterLink
+          class="link"
+          active-class="active"
+          to="/mailbox"
+        >
+          MailBox
+        </RouterLink>
+      </div>
+    </nav>
+    <div class="p-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style>
+.topbar .navigation .link {
+  @apply border-b-2 border-transparent hover:border-primary transition-colors -mb-1.5 py-1 px-2;
+  
+  &.active {
+    @apply border-primary;
+  }
+}
+</style>
---
+++ src/views/MailBoxView.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import MailItem from '@/components/MailBox/MailItem.vue';
+import { accounts, mails } from '@/data/mails';
+</script>
+
+<template>
+  <div class="md:hidden">
+    <image
+      src="/examples/mail-dark.png"
+      :width="1280"
+      :height="727"
+      alt="Mail"
+      class="hidden dark:block"
+    />
+    <image
+      src="/examples/mail-light.png"
+      :width="1280"
+      :height="727"
+      alt="Mail"
+      class="block dark:hidden"
+    />
+  </div>
+  <div class="hidden flex-col md:flex">
+    <MailItem
+      :accounts="accounts"
+      :mails="mails"
+      :nav-collapsed-size="4"
+    />
+  </div>
+</template>
