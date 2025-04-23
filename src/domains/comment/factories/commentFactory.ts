76f927c094e4ebbf9101710b6b01f374f092822a
Jan Madeyski <jan.madeyski@gmail.com>
2025-04-23 21:14:41 +0200

    Update commentFactory.ts

--- src/domains/comment/factories/commentFactory.ts
+++ src/domains/comment/factories/commentFactory.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker'
 import { formatISO } from 'date-fns'
 import { v4 } from 'uuid'
-import type { IComment } from '@/models/comment.model'
+import type { IComment } from '@/domains/comment/models/comment.model'
 
 export class CommentFactory {
   static createComment(overrides: Partial<IComment> = {}): IComment {
