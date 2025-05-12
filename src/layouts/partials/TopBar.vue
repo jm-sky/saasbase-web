2ba88c6e576f8fff436661d821ebe63eb505de95
Jan Madeyski <jan.madeyski@gmail.com>
2025-05-12 19:30:20 +0200

    Update TopBar.vue

--- src/layouts/partials/TopBar.vue
+++ src/layouts/partials/TopBar.vue
@@ -4,14 +4,16 @@ import Search from '@/components/Dashboard/Search.vue'
 import UserNav from '@/components/Layout/UserNav.vue'
 import ThemeColorButton from '@/components/LayoutConfigurator/ThemeColorButton.vue'
 import TopbarNav from './TopbarNav.vue'
+
+const SHOW_NAV = false
 </script>
 
 <template>
   <div class="border-b">
     <div class="flex h-16 items-center px-4">
-      <TopbarNav class="hidden md:block mx-6" />
+      <TopbarNav v-if="SHOW_NAV" class="hidden md:block mx-6" />
       <div class="ml-auto flex items-center gap-2">
-        <Search />
+        <Search class="hidden md:block" />
         <DarkModeButton />
         <ThemeColorButton />
         <UserNav />
