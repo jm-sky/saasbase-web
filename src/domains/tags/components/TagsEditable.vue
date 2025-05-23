361a677a81ad3240a46d3e8144c2c0d46cb39490
Jan Madeyski <jan.madeyski@gmail.com>
2025-05-24 01:50:14 +0200

    Tags editable alfa

--- src/domains/tags/components/TagsEditable.vue
+++ src/domains/tags/components/TagsEditable.vue
@@ -1,28 +1,32 @@
 <script setup lang="ts">
 import { useFilter } from 'reka-ui'
-import { computed, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { Combobox, ComboboxAnchor, ComboboxEmpty, ComboboxGroup, ComboboxInput, ComboboxItem, ComboboxList } from '@/components/ui/combobox'
 import { TagsInput, TagsInputInput, TagsInputItem, TagsInputItemDelete, TagsInputItemText } from '@/components/ui/tags-input'
 import { useTags } from '../composables/useTags'
-  
-const { availableTags } = useTags()
+
+const { availableTags, loadTags } = useTags()
 
 const modelValue = defineModel<string[]>('modelValue', {required: true})
 const open = ref(false)
 const searchTerm = ref('')
 
 const { contains } = useFilter({ sensitivity: 'base' })
-  
+
 const filteredOptions = computed(() => {
-  const options = availableTags.filter(i => !modelValue.value.includes(i.label))
-  return searchTerm.value ? options.filter(option => contains(option.label, searchTerm.value)) : options
+  const options = availableTags.value.filter(i => !modelValue.value.includes(i.name))
+  return searchTerm.value ? options.filter(option => contains(option.name, searchTerm.value)) : options
+})
+
+onMounted(async () => {
+  await loadTags()
 })
 </script>
 
 <template>
   <Combobox v-model="modelValue" v-model:open="open" :ignore-filter="true">
     <ComboboxAnchor as-child>
-      <TagsInput v-model="modelValue" class="px-2 gap-2 w-80">
+      <TagsInput v-model="modelValue" class="px-2 gap-2 w-full">
         <div class="flex gap-2 flex-wrap items-center">
           <TagsInputItem v-for="item in modelValue" :key="item" :value="item">
             <TagsInputItemText />
@@ -39,7 +43,9 @@ const filteredOptions = computed(() => {
         <ComboboxEmpty />
         <ComboboxGroup>
           <ComboboxItem
-            v-for="option in filteredOptions" :key="option.value" :value="option.label"
+            v-for="option in filteredOptions"
+            :key="option.slug"
+            :value="option.name"
             @select.prevent="(ev) => {
 
               if (typeof ev.detail.value === 'string') {
@@ -52,7 +58,7 @@ const filteredOptions = computed(() => {
               }
             }"
           >
-            {{ option.label }}
+            {{ option.name }}
           </ComboboxItem>
         </ComboboxGroup>
       </ComboboxList>
