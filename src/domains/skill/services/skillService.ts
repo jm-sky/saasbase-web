2f4687ae09d062030f4ecbe17463569a15fd4418
Jan Madeyski <jan.madeyski@gmail.com>
2025-05-30 18:52:30 +0200

    Update skillService.ts

--- src/domains/skill/services/skillService.ts
+++ src/domains/skill/services/skillService.ts
@@ -1,32 +1,38 @@
-import { type ISkill, Skill } from '@/domains/skill/models/skill.model'
+import { buildSpatieQuery } from '@/domains/shared/helpers/filtering'
+import { type ISkill } from '@/domains/skill/models/skill.model'
 import api from '@/lib/api'
 import { apiRoutesMap } from '@/lib/api/apiRoutes'
+import type { SortingState } from '@tanstack/vue-table'
+import type { FilterDefinition, IResource, IResourceCollection } from '@/domains/shared/types/resource.type'
 
-export interface ISkillGetParams {
-  categoryId?: string
-  limit?: number
-  offset?: number
+export interface ISkillFilters {
+  search?: string
+  page?: number
+  perPage?: number
+  filter?: Record<string, FilterDefinition>
+  sort?: SortingState
 }
 
 class SkillService {
-  async index(params?: ISkillGetParams): Promise<Skill[]> {
-    const response = await api.get<{ data: ISkill[] }>(apiRoutesMap.skills, { params })
-    return response.data.data.map(data => Skill.load(data))
+  async index(filters?: ISkillFilters): Promise<IResourceCollection<ISkill>> {
+    const params = buildSpatieQuery(filters ?? { filter: {} })
+    const response = await api.get<IResourceCollection<ISkill>>(apiRoutesMap.skills, { params })
+    return response.data
   }
 
-  async get(id: string): Promise<Skill> {
-    const response = await api.get<{ data: ISkill }>(`${apiRoutesMap.skills}/${id}`)
-    return Skill.load(response.data.data)
+  async get(id: string): Promise<ISkill> {
+    const response = await api.get<IResource<ISkill>>(`${apiRoutesMap.skills}/${id}`)
+    return response.data.data
   }
 
-  async create(skill: Omit<ISkill, 'id' | 'createdAt' | 'updatedAt'>): Promise<Skill> {
-    const response = await api.post<{ data: ISkill }>(apiRoutesMap.skills, skill)
-    return Skill.load(response.data.data)
+  async create(skill: Omit<ISkill, 'id' | 'createdAt' | 'updatedAt'>): Promise<ISkill> {
+    const response = await api.post<IResource<ISkill>>(apiRoutesMap.skills, skill)
+    return response.data.data
   }
 
-  async update(id: string, skill: Partial<ISkill>): Promise<Skill> {
-    const response = await api.patch<{ data: ISkill }>(`${apiRoutesMap.skills}/${id}`, skill)
-    return Skill.load(response.data.data)
+  async update(id: string, skill: Partial<ISkill>): Promise<ISkill> {
+    const response = await api.patch<IResource<ISkill>>(`${apiRoutesMap.skills}/${id}`, skill)
+    return response.data.data
   }
 
   async delete(id: string): Promise<void> {
