2273692ea056baf19495aa8b10fea13d9721dfff
Jan Madeyski <jan.madeyski@gmail.com>
2025-06-19 20:54:36 +0200

    Create firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCT3DR4BX3s6A2zcKw3wKizpHJBQjeGPWc",
+  authDomain: "saasbase-app.firebaseapp.com",
+  projectId: "saasbase-app",
+  storageBucket: "saasbase-app.firebasestorage.app",
+  messagingSenderId: "641531141485",
+  appId: "1:641531141485:web:4c1bf93936a25c061f4e64"
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
